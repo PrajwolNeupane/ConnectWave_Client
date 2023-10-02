a9ef6ed815594d4eafcac4a5047abc441603fa64
PrajwolNeupane <prajwolneupane68@gmail.com>
2023-10-02 21:09:30 +0545

    ui:working on reset password page

---
+++ app/(root)/profile/password/reset/ResetPasswordForm.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+export default function ResetPasswordForm() {
+  return (
+    <form className="w-2/3 p-5 bg-white rounded-md shadow">
+      <div>
+        <h3 className="text-xxxs font-semibold text-gray-800">Username :</h3>
+        <div className="font-semibold text-xs text-gray-800 border border-gray-400 rounded-md px-2 py-1">
+          @
+          <input
+            placeholder={"Username"}
+            // {...register("username")}
+            className="focus:outline-none"
+          />
+        </div>
+      </div>
+      <div>
+        <h3 className="text-xxxs font-semibold text-gray-800">Username :</h3>
+        <div className="font-semibold text-xs text-gray-800 border border-gray-400 rounded-md px-2 py-1">
+          @
+          <input
+            placeholder={"Username"}
+            // {...register("username")}
+            className="focus:outline-none"
+          />
+        </div>
+      </div>
+      <div>
+        <h3 className="text-xxxs font-semibold text-gray-800">Username :</h3>
+        <div className="font-semibold text-xs text-gray-800 border border-gray-400 rounded-md px-2 py-1">
+          @
+          <input
+            placeholder={"Username"}
+            // {...register("username")}
+            className="focus:outline-none"
+          />
+        </div>
+      </div>
+    </form>
+  );
+}
--- app/(root)/profile/password/reset/page.tsx
+++ app/(root)/profile/password/reset/page.tsx
@@ -1,3 +1,14 @@
+import QueryProvider from "@/Layout/Provider";
+import ResetPasswordForm from "./ResetPasswordForm";
+
 export default function Page() {
-  return <></>;
+  return (
+    <QueryProvider
+      Children={
+        <div className="flex items-center justify-center w-full bg-gray-200 h-[90.42vh]">
+          <ResetPasswordForm />
+        </div>
+      }
+    />
+  );
 }
